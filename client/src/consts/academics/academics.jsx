e2594e5359e648dbdb23654e85ff36c7cf76a438
excellencior <turjob44@gmail.com>
2025-10-17 10:46:44 +0600

    modified the academics page

--- client/src/consts/academics/academics.jsx
+++ client/src/consts/academics/academics.jsx
@@ -1,46 +1,59 @@
 const academics = [
-    {
-        id: 6,
-        year: "2020 - 2025",
-        level: "Undergraduate",
-        class: "Computer Science and Engineering",
-        institution: "Bangladesh University of Engineering & Technology, Dhaka",
-    },
-    {
-        id: 5,
-        year: "2017 - 2019",
-        level: "Higher Secondary",
-        class: "Class-11 to Class-12",
-        institution: "Shahid Syed Nazrul Islam College, Mymensingh",
-    },
-    {
-        id: 4,
-        year: "2012 - 2017",
-        level: "Secondary",
-        class: "Class-6 to Class-10",
-        institution: "Anjuman Adarsha Govt. High School, Netrakona",
-    },
-    {
-        id: 3,
-        year: "2009 - 2011",
-        level: "Primary",
-        class: "Class-2 to Class-5",
-        institution: "Anjuman Adarsha Govt. High School adj to Primay School",
-    },
-    {
-        id: 2,
-        year: "2007 - 2008",
-        level: "Primary",
-        class: "Class-1 to Class-2",
-        institution: "Mukti Precadet School",
-    },
-    {
-        id: 1,
-        year: "2005 - 2006",
-        level: "Pre-School",
-        class: "Play & Nursery",
-        institution: "Mukti Precadet School",
+  {
+    id: 5,
+    year: "2020 - 2025",
+    level: "Undergraduate",
+    class: "Computer Science and Engineering",
+    institution: "Bangladesh University of Engineering & Technology",
+    location: {
+      place: "Dhaka, Bangladesh",
+      link: "https://maps.app.goo.gl/16PysViis2NH6AE28"
     }
+  },
+  {
+    id: 4,
+    year: "2017 - 2019",
+    level: "Higher Secondary",
+    class: "Class-11 to Class-12",
+    institution: "Shahid Syed Nazrul Islam College, Mymensingh",
+    location: {
+      place: "Mymensingh, Bangladesh",
+      link: "https://maps.app.goo.gl/jvsBt8ZJEAcosFab8"
+    }
+  },
+  {
+    id: 3,
+    year: "2012 - 2017",
+    level: "Secondary",
+    class: "Class-6 to Class-10",
+    institution: "Anjuman Adarsha Govt. High School, Netrakona",
+    location: {
+      place: "Netrokona, Bangladesh",
+      link: "https://maps.app.goo.gl/CM8thbB61xsD6yuE6"
+    }
+  },
+  {
+    id: 2,
+    year: "2009 - 2011",
+    level: "Primary",
+    class: "Class-2 to Class-5",
+    institution: "Anjuman Adarsha Govt. High School adj to Primary School",
+    location: {
+      place: "Netrokona, Bangladesh",
+      link: "https://maps.app.goo.gl/CM8thbB61xsD6yuE6"
+    }
+  },
+  {
+    id: 1,
+    year: "2005 - 2008",
+    level: "Pre-School",
+    class: "Play & Nursery - Class-2",
+    institution: "Mukti Precadet School",
+    location: {
+      place: "Kalmakanda, Netrokona, Bangladesh",
+      link: "https://maps.app.goo.gl/7DYMrU5jLdcq9wSy6"
+    }
+  }
 ];
 
 export default academics;
